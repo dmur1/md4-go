eba0e01689010d69170e4ca87edad834255025f6
smiley <mail@dmur.net>
2023-12-18 14:44:55 +0000

    add a comment about the oeis sequence

--- round_3_indicies.py
+++ round_3_indicies.py
@@ -1,3 +1,6 @@
+# https://oeis.org/A030109 starting from index 16
+# 0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15
+
 for i in range(16):
     original_value = format(i, '04b')
     reversed_value_binary = format(int(original_value[::-1], 2), '04b')
